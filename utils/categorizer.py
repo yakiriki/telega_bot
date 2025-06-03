6a98bf5d317464e789721442242ab6018cc314f1
yakiriki <dimsao@gmail.com>
2025-06-03 18:51:52 +0300

    Update categorizer.py

--- utils/categorizer.py
+++ utils/categorizer.py
@@ -1,1 +1,12 @@
+def categorize_item(name: str) -> str:
+    name = name.lower()
+    if "хліб" in name or "булк" in name:
+        return "Хліб"
+    elif "вода" in name or "напій" in name:
+        return "Напої"
+    elif "сигарет" in name or "пар" in name:
+        return "Сигарети"
+    elif "томат" in name or "помідор" in name:
+        return "Овочі"
+    return "Інше"
 
