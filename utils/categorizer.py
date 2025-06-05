4bc7498998216874bfc2cd5c0e94760ac5a9f1a2
yakiriki <dimsao@gmail.com>
2025-06-05 10:10:40 +0300

    Delete utils directory

--- utils/categorizer.py
+++
@@ -1,8 +0,0 @@
-def categorize_item(name: str) -> str:
-    name = name.lower()
-    if "хліб" in name: return "Хліб"
-    if "вода" in name: return "Вода"
-    if "молоко" in name: return "Молочне"
-    if "пиво" in name: return "Алкоголь"
-    if "сигарет" in name: return "Сигарети"
-    return "Інше"
--- utils/ocr.py
+++
@@ -1,5 +0,0 @@
-
-import pytesseract
-
-def extract_text_from_image(image):
-    return pytesseract.image_to_string(image, lang='ukr+eng')
